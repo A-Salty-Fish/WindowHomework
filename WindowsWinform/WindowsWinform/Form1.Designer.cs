51acd277555968432d9203012244de73cd95ca96
A-Salty-Fish <1309032417@qq.com>
2020-10-17 16:40:31 +0800

    完成同步ping

--- WindowsWinform/WindowsWinform/Form1.Designer.cs
+++ WindowsWinform/WindowsWinform/Form1.Designer.cs
@@ -28,27 +28,29 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // inputTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(624, 65);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(164, 28);
-            this.textBox1.TabIndex = 0;
+            this.inputTextBox.Location = new System.Drawing.Point(624, 65);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(164, 28);
+            this.inputTextBox.TabIndex = 0;
             // 
-            // textBox2
+            // outputTextBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(84, 64);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(534, 323);
-            this.textBox2.TabIndex = 1;
+            this.outputTextBox.Location = new System.Drawing.Point(84, 64);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(534, 323);
+            this.outputTextBox.TabIndex = 1;
             // 
             // button1
             // 
@@ -56,8 +58,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(163, 40);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "SynButton";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -65,18 +68,29 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(163, 40);
             this.button2.TabIndex = 3;
-            this.button2.Text = "button2";
+            this.button2.Text = "AsyButton";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(624, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 40);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ClearButton";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTextBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -86,10 +100,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- WindowsWinform/WindowsWinform/Form1.cs
+++ WindowsWinform/WindowsWinform/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,41 @@ namespace WindowsWinform
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SynPing(outputTextBox, inputTextBox.Text, 9);
+        }
+        string DefaultTargetUrl = "www.whu.edu.cn";
+        private void SynPing(TextBox textBox, string targetUrl, int nums)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            // 是否使用外壳程序   
+            process.StartInfo.UseShellExecute = false;
+            // 是否在新窗口中启动该进程的值   
+            process.StartInfo.CreateNoWindow = true;
+            // 重定向输入流  
+            process.StartInfo.RedirectStandardInput = true;
+            // 重定向输出流
+            process.StartInfo.RedirectStandardOutput = true;
+            //使ping命令执行nums次
+            string strCmd = "ping " +
+                ((targetUrl == null || targetUrl == "") ? DefaultTargetUrl : targetUrl)
+                 + " -n " + nums;
+            process.Start();
+            process.StandardInput.WriteLine(strCmd);
+            process.StandardInput.WriteLine("exit");
+            // 获取输出信息   
+            textBox.Text = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            process.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = null;
         }
     }
 }
