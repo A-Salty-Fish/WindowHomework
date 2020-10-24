da93cd57353787c6890e46168905c36d36b477a5
A-Salty-Fish <1309032417@qq.com>
2020-10-24 15:26:10 +0800

    初始化类库项目

---
+++ DLLandCOM/FacFab/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FacFab
+{
+    public class MyDLLClass
+    {
+        public static long fac(long num)
+        {
+            if (num < 0) return -1;
+            long result = 1;
+            for (int i = 0; i < num; i++)
+                result = result * i;
+            return result;
+        }
+        public static long fab(long num)
+        {
+            if (num < 0) return -1;
+            if (num == 0) return 0;
+            if (num == 1) return 1;
+            int first = 0;
+            int second = 1;
+            int result = second;
+            for (int i = 1; i < num; i++)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+            }
+            return result;
+        }
+    }
+}
